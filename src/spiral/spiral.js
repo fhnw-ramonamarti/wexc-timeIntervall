2b710f8f5806ab10ca7af28ee2dc8b0cb3d0c365
Ramona Marti <ramona.marti@students.fhnw.ch>
2024-04-11 20:05:33 +0200

    fix wrong tick, add hours

--- src/spiral/spiral.js
+++ src/spiral/spiral.js
@@ -9,7 +9,7 @@ let dd = 0;
 let pathLength = 0;
 for (let i = 0; i < 9; i++) {
     let form = size * i + size;
-    pathLength += form * Math.PI;
+    pathLength += (form * Math.PI);
     if (i % 2 != 0) {
         // bottom
         xx -= size * i;
@@ -21,16 +21,18 @@ for (let i = 0; i < 9; i++) {
         i == 8 ? form / 2 : form + 5
     }" height=""${form}" xmlns="http://www.w3.org/2000/svg" id="time${i}" class=" ${
         i % 2 == 0 ? "lll" : "rrr"
-    }" style="top:${yy}px;left:${xx - (size * i) / 2 - size / 2 - dd + 2}px;width:${
-        i == 8 ? form + 5 : form * 2 + 5
-    }px;height:${form + 5}px">`;
+    }"  viewbox=" ${
+        i % 2 == 0 ? `-10 -${form + 10} ${i == 8 ? form + 20 : form * 2 + 20} ${form+10}` : `-10 0 ${i == 8 ? form + 20 : form * 2 + 20} ${form+10}`
+    }" style="top:${yy}px;left:${xx - (size * i) / 2 - size / 2 - dd}px;width:${
+        i == 8 ? form + 20 : form * 2 + 20
+    }px;height:${form + 10}px">`;
     elem += `<circle r="${form}" cx="${form + 2}" cy="0" stroke="black" fill="transparent" stroke-width="2" />`;
     elem += `<circle r="${form}" cx="${
         form + 2
-    }" cy="0" stroke="gray" fill="transparent" stroke-width="7" class="min" id="m${i}" stroke-dashoffset="0,1" />`;
+    }" cy="0" stroke="gray" fill="transparent" stroke-width="7" class="min" id="m${i}" stroke-dashoffset="0" stroke-dasharray="0,1" />`;
     elem += `<circle r="${form}" cx="${
         form + 2
-    }" cy="0" stroke="black" fill="transparent" stroke-width="13" class="hour" id="h${i}" stroke-dashoffset="0,1" />`;
+    }" cy="0" stroke="darkred" fill="transparent" stroke-width="13" class="hour" id="h${i}" stroke-dashoffset="0" stroke-dasharray="0,1" />`;
     elem += `</svg>`;
     if (i % 2 == 0) {
         // top
@@ -38,19 +40,22 @@ for (let i = 0; i < 9; i++) {
         dd = 0;
     }
     if (i == 8) {
-        pathLength -= (form * Math.PI) / 2;
+        pathLength -= ((form * Math.PI) / 2);
     }
 }
-pathLength -= 0;
+pathLength = Math.floor(pathLength);
 spiral.innerHTML = elem;
 
-let j = 1;
+let j = 0;
+let offset = 0;
 spiral.querySelectorAll("svg").forEach((e) => {
-    j *= -1;
+    j = j == 1 ? 0 : 1;
+    const rad = Math.floor(size * (e.id.substring(4)-1+2)*Math.PI);
     const pathM = e.querySelector(".min");
-    pathM.setAttribute("stroke-dasharray", `1,${pathLength / 24 / 4 - 1}`);
-    pathM.setAttribute("stroke-dashoffset", `${0}`);
+    pathM.setAttribute("stroke-dasharray", `1,${(pathLength / 24 / 4-1)}`);
+    pathM.setAttribute("stroke-dashoffset", `${-(2+offset+j*rad)}`);
     const pathH = e.querySelector(".hour");
-    pathH.setAttribute("stroke-dasharray", `0,${pathLength / 24 - 0}`);
-    // pathH.setAttribute("stroke-dashoffset", `${(pathLength / 24 / 4) * j + pathLength / 24 / 2}`);
+    pathH.setAttribute("stroke-dasharray", `2,${(pathLength / 24-2) }`);
+    pathH.setAttribute("stroke-dashoffset", `${-(2+offset+j*rad)}`);
+    offset = ((pathLength / 24) - ((rad - offset) % (pathLength / 24)));
 });
