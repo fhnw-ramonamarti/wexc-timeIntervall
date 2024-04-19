86d09cfd4267afdf77c830369af10e31974fad2a
Ramona Marti <ramona.marti@students.fhnw.ch>
2024-04-20 01:21:51 +0200

    update ui with new svg, all ui as svg import, add linking ui to fields

--- src/spiral/spiral.js
+++ src/spiral/spiral.js
@@ -1,87 +1,89 @@
-import { all, allHandles } from "./svgs.js";
+import { circles, segments, handels, numbers } from "./svgs.js";
 
 const spiral = document.querySelector("#spiralTime");
 
 // circle spiral
-const size = 25;
-const yy = spiral.clientHeight / 2;
-let xx = spiral.clientWidth / 2 - size;
-let elem = "";
-let dd = 0;
-let pathLength = 0;
-let circs = [];
-let circs2 = [];
-for (let i = 0; i < 9; i++) {
-    let form = size * i + size;
-    pathLength += form * Math.PI;
-    circs.push(pathLength);
-    circs2.push(i == 8 ? (Math.PI * form) / 2 : Math.PI * form);
-    if (i % 2 != 0) {
-        // bottom
-        xx -= size * i;
-    } else {
-        // top
-        dd = -size / 2;
-    }
-    elem += `<svg width=""${
-        i == 8 ? form / 2 : form + 5
-    }" height=""${form}" xmlns="http://www.w3.org/2000/svg" id="time${i}" class=" ${
-        i % 2 == 0 ? "lll" : "rrr"
-    }"  viewbox=" ${
-        i % 2 == 0
-            ? `-10 -${form + 10} ${i == 8 ? form + 20 : form * 2 + 20} ${form + 10}`
-            : `-10 0 ${i == 8 ? form + 20 : form * 2 + 20} ${form + 10}`
-    }" style="top:${yy}px;left:${xx - (size * i) / 2 - size / 2 - dd}px;width:${
-        i == 8 ? form + 20 : form * 2 + 20
-    }px;height:${form + 10}px;z-index:${20 - i};">`;
-    elem += `<circle r="${form / 2}" cx="${
-        form + 2
-    }" cy="0" stroke="lightgreen" fill="transparent" stroke-width="${form}" stroke-dasharray="0,1" class="mark" id="mark${i}" />`;
-    elem += `<circle r="${form}" cx="${form + 2}" cy="0" stroke="black" fill="transparent" stroke-width="2" />`;
-    elem += `<circle r="${form}" cx="${
-        form + 2
-    }" cy="0" stroke="gray" fill="transparent" stroke-width="7" class="min" id="m${i}" stroke-dashoffset="0" stroke-dasharray="0,1" />`;
-    elem += `<circle r="${form}" cx="${
-        form + 2
-    }" cy="0" stroke="darkred" fill="transparent" stroke-width="13" class="hour" id="h${i}" stroke-dashoffset="0" stroke-dasharray="0,1" />`;
-    elem += `</svg>`;
-    if (i % 2 == 0) {
-        // top
-        xx += size * i;
-        dd = 0;
-    }
-    if (i == 8) {
-        pathLength -= (form * Math.PI) / 2;
-    }
-}
-pathLength = Math.floor(pathLength - pathLength / 24 / 4);
-spiral.querySelector("#circles").innerHTML += elem;
+// const size = 25;
+// const yy = spiral.clientHeight / 2;
+// let xx = spiral.clientWidth / 2 - size;
+// let elem = "";
+// let dd = 0;
+// let pathLength = 0;
+// let circs = [];
+// let circs2 = [];
+// for (let i = 0; i < 9; i++) {
+//     let form = size * i + size;
+//     pathLength += form * Math.PI;
+//     circs.push(pathLength);
+//     circs2.push(i == 8 ? (Math.PI * form) / 2 : Math.PI * form);
+//     if (i % 2 != 0) {
+//         // bottom
+//         xx -= size * i;
+//     } else {
+//         // top
+//         dd = -size / 2;
+//     }
+//     elem += `<svg width=""${
+//         i == 8 ? form / 2 : form + 5
+//     }" height=""${form}" xmlns="http://www.w3.org/2000/svg" id="time${i}" class=" ${
+//         i % 2 == 0 ? "lll" : "rrr"
+//     }"  viewbox=" ${
+//         i % 2 == 0
+//             ? `-10 -${form + 10} ${i == 8 ? form + 20 : form * 2 + 20} ${form + 10}`
+//             : `-10 0 ${i == 8 ? form + 20 : form * 2 + 20} ${form + 10}`
+//     }" style="top:${yy}px;left:${xx - (size * i) / 2 - size / 2 - dd}px;width:${
+//         i == 8 ? form + 20 : form * 2 + 20
+//     }px;height:${form + 10}px;z-index:${20 - i};">`;
+//     elem += `<circle r="${form / 2}" cx="${
+//         form + 2
+//     }" cy="0" stroke="lightgreen" fill="transparent" stroke-width="${form}" stroke-dasharray="0,1" class="mark" id="mark${i}" />`;
+//     elem += `<circle r="${form}" cx="${form + 2}" cy="0" stroke="black" fill="transparent" stroke-width="2" />`;
+//     elem += `<circle r="${form}" cx="${
+//         form + 2
+//     }" cy="0" stroke="gray" fill="transparent" stroke-width="7" class="min" id="m${i}" stroke-dashoffset="0" stroke-dasharray="0,1" />`;
+//     elem += `<circle r="${form}" cx="${
+//         form + 2
+//     }" cy="0" stroke="darkred" fill="transparent" stroke-width="13" class="hour" id="h${i}" stroke-dashoffset="0" stroke-dasharray="0,1" />`;
+//     elem += `</svg>`;
+//     if (i % 2 == 0) {
+//         // top
+//         xx += size * i;
+//         dd = 0;
+//     }
+//     if (i == 8) {
+//         pathLength -= (form * Math.PI) / 2;
+//     }
+// }
+// pathLength = Math.floor(pathLength - pathLength / 24 / 4);
+// spiral.querySelector("#circles").innerHTML += elem;
+spiral.querySelector("#circles").innerHTML = circles;
 
 // ticks
-let j = 0;
-let offset = 0;
-let offs = [];
-spiral.querySelectorAll("svg").forEach((e) => {
-    j = j == 1 ? 0 : 1;
-    const rad = Math.floor(size * (e.id.substring(4) - 1 + 2) * Math.PI);
-    const pathM = e.querySelector(".min");
-    pathM.setAttribute("stroke-dasharray", `1,${(e.id.substring(4) == 0 ? 2 : 1) * (pathLength / 24 / 4 - 1)}`);
-    pathM.setAttribute("stroke-dashoffset", `${-(3 + offset + j * rad)}`);
-    const pathH = e.querySelector(".hour");
-    pathH.setAttribute("stroke-dasharray", `2,${pathLength / 24 - 2}`);
-    pathH.setAttribute("stroke-dashoffset", `${-(3 + offset + j * rad)}`);
-    if (e.id.substring(4) == 0) {
-        offset = pathLength / 24 / 4;
-        offs.push(offset);
-    }
-    offset = pathLength / 24 - ((rad - offset) % (pathLength / 24));
-    offs.push(offset);
-});
+// let j = 0;
+// let offset = 0;
+// let offs = [];
+// spiral.querySelectorAll("svg").forEach((e) => {
+//     j = j == 1 ? 0 : 1;
+//     const rad = Math.floor(size * (e.id.substring(4) - 1 + 2) * Math.PI);
+//     const pathM = e.querySelector(".min");
+//     pathM.setAttribute("stroke-dasharray", `1,${(e.id.substring(4) == 0 ? 2 : 1) * (pathLength / 24 / 4 - 1)}`);
+//     pathM.setAttribute("stroke-dashoffset", `${-(3 + offset + j * rad)}`);
+//     const pathH = e.querySelector(".hour");
+//     pathH.setAttribute("stroke-dasharray", `2,${pathLength / 24 - 2}`);
+//     pathH.setAttribute("stroke-dashoffset", `${-(3 + offset + j * rad)}`);
+//     if (e.id.substring(4) == 0) {
+//         offset = pathLength / 24 / 4;
+//         offs.push(offset);
+//     }
+//     offset = pathLength / 24 - ((rad - offset) % (pathLength / 24));
+//     offs.push(offset);
+// });
 
 // numbers ticks
-for (let i = 0; i <= 24; i++) {
-    spiral.querySelector("#numbs").innerHTML += `<div class="hour" id="hi${i}">${i}</div>`;
-}
+// for (let i = 0; i <= 24; i++) {
+//     spiral.querySelector("#numbs").innerHTML += `<div class="hour" id="hi${i}">${i}</div>`;
+// }
+spiral.querySelector("#numbers").innerHTML += numbers;
 
 // segments
 let blockedTime = [5.25, 6.5].map((e) => e * 4);
@@ -97,21 +99,19 @@ let currClick;
 let clicked = false;
 
 spiral.querySelectorAll(".segments").forEach((e) => {
-    e.innerHTML = all;
+    e.innerHTML = segments;
 });
-spiral.querySelectorAll(".handles").forEach((e) => {
-    e.innerHTML = allHandles;
-});
-spiral.querySelectorAll(".segments svg, .handles svg").forEach((e) => {
-    e.setAttribute("width", spiral.clientWidth);
-    e.setAttribute("height", spiral.clientHeight);
-});
-let ids = ["fragsBg", "fragsFg", "handsBg"];
+spiral.querySelector(".handles").innerHTML = handels;
+
+let ids = ["fragsBg", "fragsFg", "handsBg", "circsFg", "numbsFg", "inputsFg"].sort();
 let inxId = 0;
-spiral.querySelectorAll(".frags").forEach((e) => {
-    e.id = ids[inxId++];
-});
-spiral.querySelector("#hands").id = ids[inxId++];
+[...spiral.querySelectorAll(".container svg")]
+    .sort((a, b) => a.classList[0].localeCompare(b.classList[0]))
+    .forEach((e) => {
+        e.setAttribute("width", spiral.clientWidth);
+        e.setAttribute("height", spiral.clientHeight);
+        e.id = ids[inxId++];
+    });
 
 const handlesBg = spiral.querySelector("#handsBg");
 const segmentsFg = spiral.querySelector("#fragsFg");
@@ -124,7 +124,8 @@ let cc = 0;
         e.setAttribute("data-value", cc);
         e.setAttribute("id", `s${cc++}`);
         e.setAttribute("class", `seg`);
-        // e.setAttribute("stroke", "transparent");
+        e.setAttribute("fill", "transparent");
+        e.setAttribute("stroke", "transparent");
         if (blocked.includes(cc)) {
             e.classList.add("reserved");
         }
@@ -139,6 +140,7 @@ cc = 0;
         e.setAttribute("id", `sb${cc++}`);
         e.setAttribute("class", `seg`);
         e.setAttribute("fill-opacity", "1");
+        e.setAttribute("fill", "transparent");
         e.setAttribute("stroke", "transparent");
         if (blocked.includes(cc)) {
             e.classList.add("reserved");
@@ -171,6 +173,12 @@ cc = 0;
                 let temp = Number(e.target.getAttribute("data-value"));
                 if (start == -1 || temp > startEnd[1] || temp < startEnd[0]) {
                     start = temp;
+                    spiral.querySelectorAll(".time").forEach((e) => {
+                        e.value = `${Math.floor(start / 4)}:${
+                            ((start / 4) % 1) * 60 ? ((start / 4) % 1) * 60 : "00"
+                        }`.format();
+                    });
+                    spiral.querySelector(".dur").value = "00:00";
                     currClick = null;
                     for (let i = 0; i < 96; i++) {
                         const elemI = segmentsBg.querySelector("#sb" + i);
@@ -283,9 +291,18 @@ const fillSegs = (e) => {
         if (startEnd[0] + diff >= 0 && startEnd[1] + diff < 96) {
             start = startEnd[0] + diff;
             currClick = end;
+            startEnd = [start, startEnd[1] + diff];
+            spiral.querySelector(".dur").value = `${Math.floor((startEnd[1] - startEnd[0]) / 4) ?? "00"}:${
+                (((startEnd[1] - startEnd[0]) / 4) % 1) * 60 ? (((startEnd[1] - startEnd[0]) / 4) % 1) * 60 : "00"
+            }`.format();
+            spiral.querySelector("#startIn").value = `${Math.floor(startEnd[0] / 4)}:${
+                ((startEnd[0] / 4) % 1) * 60 ? ((startEnd[0] / 4) % 1) * 60 : "00"
+            }`.format();
+            spiral.querySelector("#endIn").value = `${Math.floor(startEnd[1] / 4)}:${
+                ((startEnd[1] / 4) % 1) * 60 ? ((startEnd[1] / 4) % 1) * 60 : "00"
+            }`.format();
             handlesBg.querySelector("#sh" + startEnd[0]).classList.add("hidden");
             handlesBg.querySelector("#sh" + startEnd[1]).classList.add("hidden");
-            startEnd = [start, startEnd[1] + diff];
             handlesBg.querySelector("#sh" + startEnd[0]).classList.remove("hidden");
             handlesBg.querySelector("#sh" + startEnd[1]).classList.remove("hidden");
             for (let i = 0; i < 96; i++) {
@@ -314,6 +331,15 @@ const fillSegs = (e) => {
             handlesBg.querySelector("#sh" + startEnd[0]).classList.remove("hidden");
             handlesBg.querySelector("#sh" + startEnd[1]).classList.remove("hidden");
         }
+        spiral.querySelector(".dur").value = `${Math.floor((startEnd[1] - startEnd[0]) / 4)}:${
+            (((startEnd[1] - startEnd[0]) / 4) % 1) * 60 ? (((startEnd[1] - startEnd[0]) / 4) % 1) * 60 : "00"
+        }`.format();
+        spiral.querySelector("#startIn").value = `${Math.floor(startEnd[0] / 4)}:${
+            ((startEnd[0] / 4) % 1) * 60 ? ((startEnd[0] / 4) % 1) * 60 : "00"
+        }`.format();
+        spiral.querySelector("#endIn").value = `${Math.floor(startEnd[1] / 4)}:${
+            ((startEnd[1] / 4) % 1) * 60 ? ((startEnd[1] / 4) % 1) * 60 : "00"
+        }`.format();
         for (let i = 0; i < 96; i++) {
             const elemI = segmentsBg.querySelector("#sb" + i);
             if (startEnd[0] <= i && startEnd[1] >= i) {
@@ -326,4 +352,9 @@ const fillSegs = (e) => {
         }
         segmentsBg.querySelector("#sb" + end).classList.add("clicked");
     }
+};
+
+String.prototype.format = function () {
+    console.log(this);
+    return (this.length == 4 ? "0" : "") + String(this);
 };
